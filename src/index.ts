f3398ad0d776f193f1dbb00e1e6da2214784043b
chankruze <chankruze@gmail.com>
2022-04-06 06:48:11 +0530

    fix: 🐛 add missing typescript & minor fix
    ✅ Closes: #3

--- src/index.ts
+++ src/index.ts
@@ -15,28 +15,39 @@ dotenv.config()
 
 let port: number = parseInt(process.env.PORT) || 6767
 
-// mongo db
-MongoClient.connect(process.env.DB_URI, {
-  maxPoolSize: 100,
-  readConcern: { level: 'majority' },
-  w: 'majority',
-  wtimeoutMS: 2500,
-  connectTimeoutMS: 2500,
-  retryWrites: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-} as MongoClientOptions)
-  .catch((err) => {
-    console.error(err.stack)
-    process.exit(1)
+// mongo db connection
+if (!process.env.DB_URI) {
+  console.error('❌ DB_URI is not defined in .env file')
+  console.error('⚠️ starting server without MongoDB connection')
+  // start listening on port
+  app.listen(port, banner(port)).on('error', (err) => {
+    if (err.message.includes('EADDRINUSE')) {
+      return app.listen(++port, banner(port))
+    }
   })
-  .then(async (client) => {
-    console.log('Connected to MongoDB 🔥🔥🔥')
-    // TODO: inject mongo client
-    // start listening on port
-    app.listen(port, banner(port)).on('error', (err) => {
-      if (err.message.includes('EADDRINUSE')) {
-        return app.listen(++port, banner(port))
-      }
+} else {
+  MongoClient.connect(process.env.DB_URI, {
+    maxPoolSize: 100,
+    readConcern: { level: 'majority' },
+    w: 'majority',
+    wtimeoutMS: 2500,
+    connectTimeoutMS: 2500,
+    retryWrites: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  } as MongoClientOptions)
+    .catch((err) => {
+      console.error(err.stack)
+      process.exit(1)
     })
-  })
+    .then(async (client) => {
+      console.log('Connected to MongoDB 🔥🔥🔥')
+      // TODO: inject mongo client
+      // start listening on port
+      app.listen(port, banner(port)).on('error', (err) => {
+        if (err.message.includes('EADDRINUSE')) {
+          return app.listen(++port, banner(port))
+        }
+      })
+    })
+}
